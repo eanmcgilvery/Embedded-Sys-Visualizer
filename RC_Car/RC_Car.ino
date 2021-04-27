c3db640cf9c52a631cfecfa5a6421c5a5bc17edc
Data Structures for CPSC 131 <eanmcgilvery@gmail.com>
2021-04-27 09:14:21 -0700

    Implemented GET Requests, Parser and interpreter for commands.

--- RC_Car/RC_Car.ino
+++ RC_Car/RC_Car.ino
@@ -3,11 +3,23 @@
 #include "vector.hpp"
 
 // WIFI Settings 
-const char ssid[] = "Emac"; //"FiOS-WDRJY";      // Change this to your Wifi settings
-const char pass[] = "eanisagod"; //way79mouth4146pay; // Change this to your Wifi settings 
+const char ssid[] = "Emac";//"Ebola-Take-The-Wheel"; //"FiOS-WDRJY";          // Change this to your Wifi settings
+const char pass[] = "eanisagod"; //"way79mouth4146pay";  //"St4yS3cure1";   // Change this to your Wifi settings 
 int status = WL_IDLE_STATUS; // Used to check connection Statuses 
 WiFiClient client;
-Vector<char> c;
+
+
+// Server Functions and Vars
+const byte serverIP[] = {107, 221, 75, 87};
+void submitGetRequest();
+void setupWifi();
+Vector<char> commands;
+Vector<char> parsedCommands;
+Vector<char> lastModified;
+
+void decideCommand(int i);
+bool parse();
+
 // Right Motor Pins
 const int enA = 10;
 const int in1 = 4;
@@ -18,10 +30,11 @@ const int enB = 9;
 const int in3 = 2;
 const int in4 = 5;
 
+int speed;
 bool notRan;
 /*================MOVEMENT FUNCTIONS===================*/
-void moveForward(int speed);
-void moveBackward(int speed);
+void moveForward();
+void moveBackward();
 void halt();
 void turnLeft();
 void turnRight();
@@ -33,37 +46,16 @@ void spinMoveBaby();
 
 void printWifiData();
 bool pingTest();
-void testServerConnect(char severname[]);
 
-
+// SETUP OF ALL VARIABLES AND CONNECTIONS
 void setup() 
 {
   Serial.begin(9600);
-  int connectionAttempts = 0;
-
-  // Try to establish a WIFI Connection
-  while ( status != WL_CONNECTED) 
-  {
-    connectionAttempts++;
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    Serial.print("Attempt #");
-    Serial.println(connectionAttempts);
-    
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
-    status = WiFi.begin(ssid, pass);
-
-    // Wait 10 seconds for connection:
-    delay(10000);
-    if(status != WL_CONNECTED)
-        Serial.println("Connection failed... Attempting Again");
-  }
-  Serial.println("CONNECTION SUCCESS!");
-
-  // Attempt to ping Google
+  setupWifi();
+  // Attempt to ping Google for a network sanity check
   while(!pingTest());
-  byte arr[4] = {54,183,187,155};
-  testServerConnect(arr);
+
+  // Setup Motor Pins to be output
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -72,28 +64,34 @@ void setup()
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
+  // Variable to ensure that the User's code is only executed once per change
   notRan = true;
+  
+  speed = 255; // NOTE: range from 0 - 255
+  for(int i = 0; i < 9; i++)
+    lastModified.push_back('0');
 }
 
 void loop() 
 {
-  int speed = 255; // NOTE: range from 0 - 255
-  delay(3000);
-  while(client.available())
-  {
-    c.push_back(client.read());
-  }
+  submitGetRequest();
   
-  if(notRan)
+  if(parse())
   {
-    Serial.println(c.getSize());
-    Serial.println("MADE IT IN HERE");
-    c.display_contents();
+    Serial.println(parsedCommands.size());
+    for(int index = 0; index < parsedCommands.size(); index++)
+    {
+      decideCommand(index);
+      delay(1000);
+    }
+    halt();
   } 
+
+  commands.clear();
   notRan = false;
 }
 
-void moveForward(int speed)
+void moveForward()
 {
   analogWrite(enA, speed);
   analogWrite(enB, speed);
@@ -105,7 +103,7 @@ void moveForward(int speed)
   digitalWrite(in4, LOW);
 }
 
-void moveBackward(int speed)
+void moveBackward()
 {
   analogWrite(enA, speed);
   analogWrite(enB, speed);
@@ -121,7 +119,7 @@ void halt()
 {
   analogWrite(enA, 0);
   analogWrite(enB, 0);
-  delay(1000);
+  delay(500);
 }
 
 void turnLeft()
@@ -183,7 +181,7 @@ void go_in_loop()
 {
   for(int i = 0; i < 4; i++)
   {
-    moveForward(255);
+    moveForward();
     delay(2000);
     turnLeft();
   }
@@ -241,20 +239,113 @@ bool pingTest()
   return true;
 }
 
-void testServerConnect(byte servername[])
+void setupWifi()
 {
-  // 54.183.187.155
-  if (client.connect({54,183,187,155}, 80)) 
+ int connectionAttempts = 0;
+
+  // Try to establish a WIFI Connection
+  while ( status != WL_CONNECTED) 
   {
-      Serial.println("Connected to AWS Server!");
-      Serial.println("Getting Data from server...");
-      //Serial.println("Pushing Data to server...");
-      client.println("GET /rc.txt HTTP/1.0");
-      //client.println("POST /var/www/html/testText.txt HTTP/1.1");
-      client.println();
+    connectionAttempts++;
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    Serial.print("Attempt #");
+    Serial.println(connectionAttempts);
+    
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+
+    // Wait 10 seconds for connection:
+    delay(10000);
+    if(status != WL_CONNECTED)
+        Serial.println("Connection failed... Attempting Again");
+  }
+  Serial.println("CONNECTION SUCCESS!");
+  
+}
+
+void submitGetRequest()
+{
+  setupWifi();
+  if (client.connect(serverIP, 80)) 
+  {
+    Serial.println("Connected to Server!");
+    Serial.println("Getting Commands from server...");
+    client.println("GET / HTTP/1.1");
+    client.println();
+    
+    // Delay the program execution so we can wait for a response from 
+    //  the server.
+    delay(2000);
+    while(client.available())
+    {      
+      commands.push_back(client.read());
+     // char c = client.read();
+     // Serial.print(c);
+    } 
+    commands.display_contents();
   }
   else
   {
       Serial.println("SERVER CONNECTION FAILED");
   }
+  client.stop();
+}
+
+bool parse()
+{   
+  parsedCommands.clear();
+  bool flag = false;
+  Serial.println();
+  Serial.println("COMMAND VEC SIZE");
+  Serial.println(commands.size());
+  for(int i = 0; i < commands.size(); i++)
+  {    
+    // Only go through and grab the commands from the Message
+    if(commands[i] == '>' && !flag)
+    {
+      Serial.print("Commands:");
+      Serial.println(commands[i]);
+      // We saw the first '>'
+      flag = true;
+      i++;
+      // Begin by grabbing timestamp
+      for(; commands[i] != '<'; i++)
+      {
+        parsedCommands.push_back(commands[i]);
+      }
+
+      Serial.println("PARSE SIZE: " + parsedCommands.size());
+      // If the message is new, update our Timestamp and continue parsing
+      if(parsedCommands > lastModified)
+      {
+        lastModified = parsedCommands;
+        lastModified.display_contents();
+      }
+      // Message isn't new, so return 
+      else
+        return false;
+
+      // Clear our the Vector so we can use it to just hold the Movement commands
+      parsedCommands.clear();
+      i++;
+    }
+
+    if(flag)
+      parsedCommands.push_back(commands[i]);
+  }
+    parsedCommands.display_contents();
+    return true;
+}
+
+void decideCommand(int i)
+{
+  if(parsedCommands[i] == 'R')
+    turnRight();
+  else if (parsedCommands[i] == 'L')
+    turnLeft();   
+  else if (parsedCommands[i] == 'F')
+    moveForward();
+  else if (parsedCommands[i] == 'B')
+    moveBackward();
 }
